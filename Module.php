5009372fa111d9bca5641e3c44e57fe638781ddd
pglushchenko <petr.glushchenko@aroma-zone.com>
2014-10-01 11:43:42 +0200

    Initial commit

---
+++ Module.php
@@ -0,0 +1,30 @@
+<?php
+namespace zPetr\Transliteration;
+
+/**
+ * ZF2 module
+ */
+class Module
+{
+    /**
+     * Retrieve autoloader configuration
+     *
+     * @return array
+     */
+    public function getAutoloaderConfig()
+    {
+        return array('Zend\Loader\StandardAutoloader' => array('namespaces' => array(
+            __NAMESPACE__ => __DIR__ . '/src/',
+        )));
+    }
+
+    /**
+     * Retrieve module configuration
+     *
+     * @return array
+     */
+    public function getConfig()
+    {
+        return include __DIR__ . '/config/module.config.php';
+    }
+}
